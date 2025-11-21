44af0e5a106481aeb5152e4a3e21ed34cd4b34fa
Javier Uncos <javiersrfc30@gmail.com>
2025-11-21 02:01:05 -0300

    agregamos formatos de imagenes compatibles a multer

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import * as dotenv from 'dotenv';
 import app from './app.js';
 import connectDb from './config/db.js';
-console.log('API KEY:', process.env.CLOUDINARY_API_KEY);
-console.log('API SECRET:', process.env.CLOUDINARY_API_SECRET);
+
 dotenv.config();
 
 //configuramos el puerto
